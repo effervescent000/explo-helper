e22e15baa34fd1b9ec4a1935c43edee75cfff5e6
effervescent000 <kislio01@gmail.com>
2024-06-11 03:38:00 -0400

    add a couple planet classes

--- values.py
+++ values.py
@@ -9,8 +9,14 @@ ROCKY = "Rocky body"
 ICY = "Icy body"
 ROCKY_ICE = "Rocky ice body"
 WATER_WORLD = "Water world"
+GAS_GIANT_I = "Sudarsky class I gas giant"
+GAS_GIANT_II = "Sudarsky class II gas giant"
+GAS_GIANT_WITH_WATER = "Gas giant with water based life"
 
 VALUES = {
+    GAS_GIANT_I: {BASE: 4_000},
+    GAS_GIANT_II: {BASE: 28_000},
+    GAS_GIANT_WITH_WATER: {BASE: 900},
     HMCB: {BASE: 14_000, TERRAFORMABLE: 164_000},
     ICY: {BASE: 500},
     ROCKY: {BASE: 500, TERRAFORMABLE: 130_000},
