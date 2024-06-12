4bb3da001e9245376b8956e908f0721e2cf46b59
effervescent000 <kislio01@gmail.com>
2024-06-12 10:27:23 -0400

    improve test

--- signals/test_signals.py
+++ signals/test_signals.py
@@ -6,5 +6,5 @@ from signals.signals import get_possible_bio_signals
 def test_get_possible_bio_signals(testing_galaxy: Galaxy) -> None:
     planet = planet_factory(atmosphere="Ammonia", gravity=0.26)
     result = get_possible_bio_signals(planet)
-    filtered = [x for x in result if x.genus == "Concha" and x.species == "aureolas"]
+    filtered = [x for x in result if x.genus == "Concha"]
     assert len(filtered) == 1
